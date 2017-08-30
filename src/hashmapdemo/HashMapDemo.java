1e4aace066d6bfd1a72ad720929f491bfc010f41
faintdream <dubey.akash@gmail.com>
2017-08-30 13:23:04 +0530

    This program demonstrates use of hashmap .

---
+++ src/hashmapdemo/HashMapDemo.java
@@ -0,0 +1,25 @@
+/*
+ * In this program we demo use of HashMap to store key value pair
+ * of an employee_name and employee_code , and display just the values for each key
+ */
+package hashmapdemo;
+
+import java.util.HashMap; //importing hashmap class
+
+
+public class HashMapDemo {              //main class 
+
+    public static HashMap<Integer,String> map=new HashMap<>();   //declaring  a hashmap  : map with key value pair
+                                                                 // of Integer and String
+    public static void main(String[] args) {
+            map.put(101, "Akash Dubey");                        //adding key, value to map
+            map.put(102, "Sajid Khursi");                       // "-"
+            map.put(103, "Shabana Janeman Khan");               // "-"
+            map.put(110, "Anthony Gonzalvez tiwari");           //adding key, value to map
+
+            for(Integer item:map.keySet()){                     //looping over hasmap
+                System.out.println(""+ map.get(item));          //feding key to print its value
+            }
+    }
+    
+}
